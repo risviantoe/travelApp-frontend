d2a4db0ccf6956924bf02d19a4f61fc989917150
Edwin Risvianto <risviantoe@gmail.com>
2022-08-28 18:01:46 +0700

    create button component

---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,29 @@
+import react from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Button from '.';
+
+test('Should not allow click button if isDisabled is present', () => {
+	const { container } = render(<Button isDisabled></Button>);
+	expect(container.querySelector('span')).toHaveClass('disabled');
+});
+
+test('Should render loading/spinner', () => {
+	const { container, getByText } = render(<Button isLoading></Button>);
+	expect(screen.getByText(/loading/i)).toBeInTheDocument();
+	expect(container.querySelector('span')).toBeInTheDocument();
+});
+
+test('Should render <a> tag', () => {
+	const { container } = render(<Button type="link" isExternal></Button>);
+	expect(container.querySelector('a')).toBeInTheDocument();
+});
+
+test('Should render <Link> component', () => {
+	const { container } = render(
+		<Router>
+			<Button href="" type="link"></Button>
+		</Router>
+	);
+	expect(container.querySelector('a')).toBeInTheDocument();
+});
